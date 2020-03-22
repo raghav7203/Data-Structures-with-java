e64be456d53e35c1c588b1a7a78a778b9726acfb
raghav <raghavsharma7203@gmail.com>
2020-03-22 19:50:50 +0530

    DS

---
+++ BinaryTree/binaryTree5.java
@@ -0,0 +1,33 @@
+//depth first preorder traversal
+class node{
+   int data;
+   node left,right;
+      public node(int d){
+         data=d;
+	 left=right=null;
+      }
+}
+class binaryTree5{
+ node root=null;
+ public void display(){
+   display(root);
+ }
+ public void display(node root){
+    if(root==null)
+	return;
+    else{
+      System.out.println(root.data);
+      display(root.left);
+      display(root.right);
+    }
+ }
+ public static void main(String args[]){
+    binaryTree5 tree=new binaryTree5();
+	tree.root=new node(1);
+	tree.root.left=new node(2);
+	tree.root.right=new node(3);
+	tree.root.left.left=new node(4);
+	tree.root.left.right=new node(5);
+     tree.display();	
+ }
+}
