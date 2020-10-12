29e29b44d444d18f7977d45066b836989a27d2e0
Raghav Sharma <48362662+raghav7203@users.noreply.github.com>
2020-10-12 22:37:35 +0530

    DS

---
+++ GFG/LinkedList/sort_list_of_0_1_2.java
@@ -0,0 +1,145 @@
+/*
+Given a linked list of N nodes where nodes can contain values 0s, 1s, and 2s only. The task is to segregate 0s, 1s, and 2s linked list such that all zeros segregate to head side, 2s at the end of the linked list, and 1s in the mid of 0s and 2s.
+
+Example 1:
+
+Input:
+N = 8
+value[] = {1,2,2,1,2,0,2,2}
+Output: 0 1 1 2 2 2 2 2
+Explanation: All the 0s are segregated
+to the left end of the linked list,
+2s to the right end of the list, and
+1s in between.
+Example 2:
+
+Input:
+N = 4
+value[] = {2,2,0,1}
+Output: 0 1 2 2
+Explanation: After arranging all the
+0s,1s and 2s in the given format,
+the output will be 0 1 2 2.
+Your Task:
+The task is to complete the function segregate() which segregates the nodes in the linked list as asked in the problem statement and returns the head of the modified linked list. The printing is done automatically by the driver code.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= N <= 103
+*/
+
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new LinkedList().segregate(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class LinkedList
+{
+    static Node segregate(Node head)
+    {
+        // add your code here
+        int z=0, o=0, t=0;
+        Node current=head;
+        while(current!=null){
+            if(current.data==0)
+                z++;
+            else if(current.data==1)
+                o++;
+            else
+                t++;
+            current=current.next;
+        }
+        current=head;
+        while(z>0){
+            current.data=0;
+            current=current.next;
+            z--;
+        }
+        while(o>0){
+            current.data=1;
+            current=current.next;
+            o--;
+        }
+        while(t>0){
+            current.data=2;
+            current=current.next;
+            t--;
+        }
+        return head;
+    }
+}
+
+
+
