5a39c64a93e2b3e97ab9b52b24e11b1235f18da5
Raghav Sharma <48362662+raghav7203@users.noreply.github.com>
2020-10-31 13:29:22 +0530

    DS

---
+++ GFG/Graph/dfs_of_graph.java
@@ -0,0 +1,122 @@
+/*
+Given a connected undirected graph. Perform a Depth First Traversal of the graph.
+Note: Use recursive approach.
+
+Input:
+The first line of the input contains an integer 'T' denoting the number of test cases. Then 'T' test cases follow. Each test case consists of two lines. Description of testcases is as follows: The First line of each test case contains two integers 'N' and 'E'  which denotes the no of vertices and no of edges respectively. The Second line of each test case contains 'E'  space separated pairs u and v denoting that there is a edge from u to v .
+
+Output:
+For each testcase, print the nodes while doing DFS starting from node 0.
+
+Your task:
+You don't need to read input or print anything. Your task is to complete the function dfs() which takes the Graph and the number of vertices as inputs and returns a list containing the DFS Traversal of the graph starting from the 0th node.
+
+Expected Time Complexity: O(V + E).
+Expected Auxiliary Space: O(V).
+
+Constraints:
+1 <= T <= 100
+2 <= N <= 104
+1 <= E <= (N*(N-1))/2
+Graph doesn't contain multiple edges and self loops.
+
+Example:
+Input:
+2
+5 4
+0 1 0 2 0 3 2 4
+4 3
+0 1 1 2 0 3
+
+Output:
+0 1 2 4 3   
+0 1 2 3
+
+Explanation:
+Testcase 1:
+0 is connected to 1 , 2 , 3
+1 is connected to 0
+2 is connected to 0 and 4
+3 is connected to 0
+4 is connected to 2
+so starting from 0 , dfs will be 0 1 2 4 3.
+*/
+
+
+//Initial Template for Java
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t =sc.nextInt();
+        while(t-- > 0)
+        {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int nov = sc.nextInt();
+            int edg  =sc.nextInt();
+            for(int i = 0; i < nov; i++)
+                list.add(i, new ArrayList<Integer>());
+                
+            for(int i = 1; i <= edg; i++)
+            {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+                list.get(v).add(u);
+            }
+            
+            ArrayList<Integer> res = new Traversal().dfs(list, nov);
+            for (int i = 0; i < res.size (); i++) 
+                System.out.print (res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+/*
+g : adjacency list of graph
+N : number of vertices
+
+return a list containing the DFS traversal of the given graph
+*/
+
+class Traversal
+{
+    static ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> g, int N)
+    {
+       // add your code here
+       boolean v[]=new boolean[N];
+       ArrayList<Integer> a=new ArrayList<>();
+       dfs(0, a, v, g);
+       return a;
+    }
+    static void dfs(int node, ArrayList<Integer> a, boolean v[], ArrayList<ArrayList<Integer>> g){
+        v[node]=true;
+        a.add(node);
+        for(int i:g.get(node)){
+            if(!v[i]){
+                dfs(i, a, v, g);
+            }
+        }
+        // for(int i=0;i<g.get(node).size();i++){
+        //     if(!v[g.get(node).get(i)]){
+        //         dfs(g.get(node).get(i), a, v, g);
+        //     }
+        // }
+    }
+}
+
+
