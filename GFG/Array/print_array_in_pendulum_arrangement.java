cdcc5fd874f9f539e5911c66b05b5adb0f8da0dc
Raghav Sharma <48362662+raghav7203@users.noreply.github.com>
2020-12-13 11:27:08 +0530

    DS

---
+++ GFG/Array/print_array_in_pendulum_arrangement.java
@@ -0,0 +1,63 @@
+/*
+Write a program to input a list of n integers in an array and arrange them in a way similar to the to-and-fro movement of a Pendulum.
+
+The minimum element out of the list of integers, must come in center position of array. If there are even elements, then minimum element should be moved to (n-1)/2 index (considering that indexes start from 0)
+The next number (next to minimum) in the ascending order, goes to the right, the next to next number goes to the left of minimum number and it continues like a Pendulum.
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains an integer n denoting the size of the array. Then next line contains N space separated integers forming the array.
+
+Output:
+Output the array in Pendulum Arrangement.
+
+Constraints:
+1<=T<=500
+1<=N<=100
+1<=a[i]<=1000
+
+Example:
+Input:
+2
+5
+1 3 2 5 4
+5
+11 12 31 14 5
+
+Output:
+5 3 1 2 4
+31 12 5 11 14
+*/
+
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner s=new Scanner(System.in);
+		int test=s.nextInt();
+		for(int t=1;t<=test;t++){
+		    int n=s.nextInt();
+		    int a[]=new int[n];
+		    for(int i=0;i<n;i++)
+		        a[i]=s.nextInt();
+		    Arrays.sort(a);
+		    if(n%2==0){
+		        for(int i=a.length-2;i>=0;i=i-2){
+		            System.out.print(a[i]+" ");
+		        }
+		        for(int i=1;i<a.length;i=i+2){
+		            System.out.print(a[i]+" ");
+		        }
+		    }
+		    else{
+		        for(int i=a.length-1;i>=0;i=i-2){
+    		        System.out.print(a[i]+" ");
+    		    }
+    		    for(int i=1;i<a.length;i=i+2){
+    		        System.out.print(a[i]+" ");
+    		    }
+		    }
+		    System.out.println();
+		}
+	}
+}
